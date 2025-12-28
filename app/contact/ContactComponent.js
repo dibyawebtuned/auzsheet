d7567e0f35206cad2e4cd71f9f979186eb8a654f
dibyamaharzan26 <dibyamaharzan26@gmail.com>
2025-12-28 10:32:52 +0545

    Contact Us Page --> Animation

--- app/contact/ContactComponent.js
+++ app/contact/ContactComponent.js
@@ -1,8 +1,12 @@
 "use client"
-import React from 'react'
+import React, { useEffect } from 'react'
+import Image from 'next/image'
 import Link from 'next/link'
 import { Home, Phone, Mail } from "lucide-react"
 import ContactSection from '@/components/landing/ContactSection'
+
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const ContactComponent = () => {
   return (
@@ -14,12 +18,12 @@ const ContactComponent = () => {
             {/*  */}
             <div className='flex flex-col sm:flex-row sm:items-center gap-[15px] sm:gap-[30px]'>
               <div className='flex-1'>
-                <h2 className='host-grotesk text-black text-3xl sm:text-4xl lg:text-[43px] host-grotesk-semibold'>
+                <h2 className='host-grotesk text-black text-3xl sm:text-4xl lg:text-[43px] host-grotesk-semibold' data-aos="fade-up">
                   Contact Us
                 </h2>
               </div>
 
-              <div className='flex-1'>
+              <div className='flex-1' data-aos="fade-left" data-aos-delay="100">
                 <span className='auz_text_gray w-[60%] host-grotesk host-grotesk-semibold text-base sm:text-[17px] lg:text-[18px] text-center leading-5'>
                   Request a fast, obligation-free quote from Auzsheet Roofing and Guttering.
                   We proudly service Newcastle, Central Coast and Hunter Regions delivering
@@ -31,7 +35,7 @@ const ContactComponent = () => {
             {/* Contact Informations */}
             <div className="flex flex-col md:flex-row justify-center items-start md:items-stretch">
               {/* Visit Us */}
-              <div className="flex-1 flex flex-col gap-4 sm:gap-7 items-center text-center p-6 sm:border-r sm:border-[#505050]">
+              <div className="flex-1 flex flex-col gap-4 sm:gap-7 items-center text-center p-6 sm:border-r sm:border-[#505050]" data-aos="fade-up">
                 <div className='bg-black w-12 h-12 rounded-[10px] flex items-center justify-center'>
                   <Home className="text-[#faf3e9] w-6 h-6" />
                 </div>
@@ -46,7 +50,7 @@ const ContactComponent = () => {
               </div>
 
               {/* Call Us */}
-              <div className="flex-1 flex flex-col gap-4 sm:gap-7 items-center text-center p-6 sm:border-r sm:border-[#505050]">
+              <div className="flex-1 flex flex-col gap-4 sm:gap-7 items-center text-center p-6 sm:border-r sm:border-[#505050]" data-aos="fade-up" data-aos-delay="100">
                 <div className='bg-black w-12 h-12 rounded-[10px] flex items-center justify-center'>
                   <Phone className="text-[#faf3e9] w-6 h-6" />
                 </div>
@@ -61,7 +65,7 @@ const ContactComponent = () => {
               </div>
 
               {/* Contact Us */}
-              <div className="flex-1 flex flex-col gap-4 sm:gap-7 items-center text-center p-6">
+              <div className="flex-1 flex flex-col gap-4 sm:gap-7 items-center text-center p-6" data-aos="fade-up" data-aos-delay="150">
                 <div className='bg-black w-12 h-12 rounded-[10px] flex items-center justify-center'>
                   <Mail className="text-[#faf3e9] w-6 h-6" />
                 </div>
@@ -77,7 +81,7 @@ const ContactComponent = () => {
             </div>
 
             {/* Map */}
-            <div className="w-full h-96 rounded-4xl overflow-hidden">
+            <div className="auz_bg w-full h-96 rounded-4xl overflow-hidden" data-aos="fade-up" data-aos-delay="200">
               <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3153.0862765739286!2d144.96305831550448!3d-37.81361197975173!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad642af0f11fd81%3A0xf577b6d603a2f0f1!2sFederation%20Square!5e0!3m2!1sen!2sau!4v1692958329111!5m2!1sen!2sau"
                 width="100%"
